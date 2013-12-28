0f71e3a3c6330dbec719f09483985d8b3ffe38f7
Danilo Kusanovic <danilo@danilo.cm.vtr.net>
2013-12-28 12:04:51 -0300

    Normal vector computation, local stiffness matrix computation and COO format of the stiffness matrix were implemented

--- setStiffnessMatrix.cpp
+++ setStiffnessMatrix.cpp
@@ -1,22 +1,23 @@
 //====================================================================================================================================
 // IMPLEMENTATION FILE: "setForceVector"
 //====================================================================================================================================
-// Syntax      : setStiffnessMatrix(Coordinates, Elements, GaussPoints,Force,Nelems,Ngauss)
+// Syntax      : setStiffnessMatrix(Coordinates, Elements, Stiffness,row,col,Nelems)
 //------------------------------------------------------------------------------------------------------------------------------------
 // Purpose     : Computes the stiffness matrix. 
 //------------------------------------------------------------------------------------------------------------------------------------
 // Input       : Coordinates : List of coordinate values                          [Nnodes,3]
 //               Elements    : List of elements values                            [Nelems,3]
-//               GaussPoints : List of Gauss Integration values                   [Ngauss,3]
+//               Stiffness   : List of Stiffness values                           [Nelems,1]
+//               row         : List of indeces i for the K matrix                 [Nelems,1]
+//               col         : List of indeces j for the K matrix                 [Nelems,1]
 //               Nelems      : Number of elements                                 [1,1]
-//               Ngauss      : Number of integration points                       [1,1]
 //------------------------------------------------------------------------------------------------------------------------------------
-// Output      : Force       : Force vector                                       [Nnodes,1]
+// Output      : Stiffness   : Stiffness matrix values                            [Nelems,1]
 //------------------------------------------------------------------------------------------------------------------------------------
 // Folder      : 
 //------------------------------------------------------------------------------------------------------------------------------------
 // Description : <Stiffness Matrix>
-// Keywords    : <Force,RHS>
+// Keywords    : <Stiffness,Assembly>
 // Version     : <>
 //====================================================================================================================================
 // Written by Danilo S. Kusanovic, December 2013
@@ -25,72 +26,89 @@
 
   #include <cmath>
 
-//------------------------------------------------------------------------------------------------------------------------------------
-// COMPUTES THE STIFFNESS MATRIX: 
-//------------------------------------------------------------------------------------------------------------------------------------
-   void setStiffnessMatrix(double** &Coordinates, int** &Elements, double** &GaussPoints, int** &Restraints, double* &Force, int Nnodes, 
-                           int Nelems, int Ngauss){
-        
-   //Element Coordinates:
-     double xi, eta, zeta;  
-
-   //Element properties: 
-     double Volume, Value, Integral[4];
-
-   //Side vector components:
+   void setStiffnessMatrix(double** &Coordinates, int** &Elements, double* &Stiffness, int* &row, int* &col, int Nelems){
+                                     
+   //Element properties:
+     double Volume, nx[4], ny[4], nz[4];
      double ux, uy, uz, vx, vy, vz, wx, wy, wz;
 
-     for(int i = 0; i < Nelems; i++){
+     int count = 0;
+     for(int k = 0; k < Nelems; k++){
 
       //------------------------------------------------------------------------------------------------------------------------------
-      // Computes Tetrahedron volume given four coordinates:
+      // LOCAL NORMAL VECTOR COMPUTATION:
       //------------------------------------------------------------------------------------------------------------------------------
-         ux = Coordinates[Elements[i][1]][0] - Coordinates[Elements[i][0]][0];
-         uy = Coordinates[Elements[i][1]][1] - Coordinates[Elements[i][0]][1];
-         uz = Coordinates[Elements[i][1]][2] - Coordinates[Elements[i][0]][2];
+         ux = Coordinates[Elements[k][3]][0] - Coordinates[Elements[k][1]][0];
+         uy = Coordinates[Elements[k][3]][1] - Coordinates[Elements[k][1]][1];
+         uz = Coordinates[Elements[k][3]][2] - Coordinates[Elements[k][1]][2];
 
-         vx = Coordinates[Elements[i][2]][0] - Coordinates[Elements[i][0]][0];
-         vy = Coordinates[Elements[i][2]][1] - Coordinates[Elements[i][0]][1];
-         vz = Coordinates[Elements[i][2]][2] - Coordinates[Elements[i][0]][2];
+         vx = Coordinates[Elements[k][2]][0] - Coordinates[Elements[k][1]][0];
+         vy = Coordinates[Elements[k][2]][1] - Coordinates[Elements[k][1]][1];
+         vz = Coordinates[Elements[k][2]][2] - Coordinates[Elements[k][1]][2];
 
-         wx = Coordinates[Elements[i][3]][0] - Coordinates[Elements[i][0]][0];
-         wy = Coordinates[Elements[i][3]][1] - Coordinates[Elements[i][0]][1];
-         wz = Coordinates[Elements[i][3]][2] - Coordinates[Elements[i][0]][2];
+         nx[0] = (uy*vz - vy*uz)/2; ny[0] = (uz*vx - vz*ux)/2; nz[0] = (ux*vy - vx*uy)/2;
 
-         Volume = fabs(ux*(vy*wz - vz*wy) - uy*(vx*wz - vz*wx) + uz*(vx*wy - vy*wx))/6;
+         ux = Coordinates[Elements[k][2]][0] - Coordinates[Elements[k][0]][0];
+         uy = Coordinates[Elements[k][2]][1] - Coordinates[Elements[k][0]][1];
+         uz = Coordinates[Elements[k][2]][2] - Coordinates[Elements[k][0]][2];
+
+         vx = Coordinates[Elements[k][3]][0] - Coordinates[Elements[k][0]][0];
+         vy = Coordinates[Elements[k][3]][1] - Coordinates[Elements[k][0]][1];
+         vz = Coordinates[Elements[k][3]][2] - Coordinates[Elements[k][0]][2];
+
+         nx[1] = (uy*vz - vy*uz)/2; ny[1] = (uz*vx - vz*ux)/2; nz[1] = (ux*vy - vx*uy)/2;
+
+         ux = Coordinates[Elements[k][3]][0] - Coordinates[Elements[k][0]][0];
+         uy = Coordinates[Elements[k][3]][1] - Coordinates[Elements[k][0]][1];
+         uz = Coordinates[Elements[k][3]][2] - Coordinates[Elements[k][0]][2];
+
+         vx = Coordinates[Elements[k][1]][0] - Coordinates[Elements[k][0]][0];
+         vy = Coordinates[Elements[k][1]][1] - Coordinates[Elements[k][0]][1];
+         vz = Coordinates[Elements[k][1]][2] - Coordinates[Elements[k][0]][2];
+
+         nx[2] = (uy*vz - vy*uz)/2; ny[2] = (uz*vx - vz*ux)/2; nz[2] = (ux*vy - vx*uy)/2;
+
+         ux = Coordinates[Elements[k][1]][0] - Coordinates[Elements[k][0]][0];
+         uy = Coordinates[Elements[k][1]][1] - Coordinates[Elements[k][0]][1];
+         uz = Coordinates[Elements[k][1]][2] - Coordinates[Elements[k][0]][2];
+
+         vx = Coordinates[Elements[k][2]][0] - Coordinates[Elements[k][0]][0];
+         vy = Coordinates[Elements[k][2]][1] - Coordinates[Elements[k][0]][1];
+         vz = Coordinates[Elements[k][2]][2] - Coordinates[Elements[k][0]][2];
+
+         nx[3] = (uy*vz - vy*uz)/2; ny[3] = (uz*vx - vz*ux)/2; nz[3] = (ux*vy - vx*uy)/2;
 
       //------------------------------------------------------------------------------------------------------------------------------
-      // Performs Gaussian Integration over the element:
+      // LOCAL ELEMENT VOLUME COMPUTATION:
       //------------------------------------------------------------------------------------------------------------------------------
-         Integral[0] = 0.0; Integral[1] = 0.0; 
-         Integral[2] = 0.0; Integral[3] = 0.0;
+         ux = Coordinates[Elements[k][1]][0] - Coordinates[Elements[k][0]][0];
+         uy = Coordinates[Elements[k][1]][1] - Coordinates[Elements[k][0]][1];
+         uz = Coordinates[Elements[k][1]][2] - Coordinates[Elements[k][0]][2];
 
-         for(int j = 0; j < Ngauss; j++){
+         vx = Coordinates[Elements[k][2]][0] - Coordinates[Elements[k][0]][0];
+         vy = Coordinates[Elements[k][2]][1] - Coordinates[Elements[k][0]][1];
+         vz = Coordinates[Elements[k][2]][2] - Coordinates[Elements[k][0]][2];
 
-          //Transformed triangular Coordinates:
-             xi   = Coordinates[Elements[i][0]][0]*GaussPoints[j][1] + Coordinates[Elements[i][1]][0]*GaussPoints[j][2] + 
-                    Coordinates[Elements[i][2]][0]*GaussPoints[j][3] + Coordinates[Elements[i][3]][0]*GaussPoints[j][4];
-             eta  = Coordinates[Elements[i][0]][1]*GaussPoints[j][1] + Coordinates[Elements[i][1]][1]*GaussPoints[j][2] + 
-                    Coordinates[Elements[i][2]][1]*GaussPoints[j][3] + Coordinates[Elements[i][3]][1]*GaussPoints[j][4];
-             zeta = Coordinates[Elements[i][0]][2]*GaussPoints[j][1] + Coordinates[Elements[i][1]][2]*GaussPoints[j][2] + 
-                    Coordinates[Elements[i][2]][2]*GaussPoints[j][3] + Coordinates[Elements[i][3]][2]*GaussPoints[j][4];
+         wx = Coordinates[Elements[k][3]][0] - Coordinates[Elements[k][0]][0];
+         wy = Coordinates[Elements[k][3]][1] - Coordinates[Elements[k][0]][1];
+         wz = Coordinates[Elements[k][3]][2] - Coordinates[Elements[k][0]][2];
 
-          //Force function evaluated at each triangular coordinates:
-            Value = GaussPoints[j][0]*getForce(xi,eta,zeta);
-
-          //Integration evaluated at each triangular Coordinates:
-            Integral[0] += Value*(1.0 - GaussPoints[j][2] - GaussPoints[j][3] - GaussPoints[j][4]);
-            Integral[1] += Value*(GaussPoints[j][2]);
-            Integral[2] += Value*(GaussPoints[j][3]);
-            Integral[3] += Value*(GaussPoints[j][4]);
-            
-         }
+         Volume = 1.5*fabs(ux*(vy*wz - vz*wy) - uy*(vx*wz - vz*wx) + uz*(vx*wy - vy*wx));
 
       //------------------------------------------------------------------------------------------------------------------------------
-      // Force value at each degree of freedom:
+      // COMPUTES LOCAL ELEMENT STIFFNESS MATRIX:
       //------------------------------------------------------------------------------------------------------------------------------
-         for(int j = 0; j < 4; j++){
-             Force[Elements[i][j]] += Volume*Integral[j];
+         for(int i = 0; i < 4; i++){
+             for(int j = 0; j < 4; j++){
+
+               //COO Stiffness Matix format:
+                 row[count]       = Elements[k][i];
+                 col[count]       = Elements[k][j];
+                 Stiffness[count] = (nx[i]*nx[j] + ny[i]*ny[j] + nz[i]*nz[j])/Volume;
+
+               //Increases index for COO format:
+                 count = count + 1;
+             }
          }
 
      }
