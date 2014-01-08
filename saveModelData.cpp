4f7d3387b304b0154fefce64c30a00b6e428db72
Danilo Kusanovic <danilo@danilo.cm.vtr.net>
2014-01-08 12:31:51 -0300

    New commented lines were added and the header was updated

--- saveModelData.cpp
+++ saveModelData.cpp
@@ -1,5 +1,5 @@
 //====================================================================================================================================
-// IMPLEMENTATION FILE: "setModelData"
+// IMPLEMENTATION FILE: "saveModelData"
 //====================================================================================================================================
 // Syntax      : saveModelData(PATH,Coordinates,Elements,GaussPoints,Restraints,Constraints,Dofs,Nnodes,Nelems,Ngauss,Nrestr,Nconst)
 //------------------------------------------------------------------------------------------------------------------------------------
@@ -9,10 +9,16 @@
 //               Coordinates : List of coordinate values                          [Nnodes,3]
 //               Elements    : List of elements values                            [Nelems,3]
 //               GaussPoints : List of Gauss Integration values                   [Ngauss,3]
+//               Restraints  : Restrained nodal values                            [Nrestr,3]
+//               Force       : Force vector values                                [Nnodes,1]
+//               Dofs        : Free degree of freedom numbering                   [Nnodes,1]
+//               Nnodes      : Number of total nodes                              [1,1] 
+//               Nelems      : Number of total elements                           [1,1] 
+//               Nrestr      : Number of restrained nodes                         [1,1]
 //------------------------------------------------------------------------------------------------------------------------------------
-// Output      : Coordinates : Updated list of coordinate values                  [Nnodes,3]
-//               Elements    : Updated list of element values                     [Nelems,3]
-//               GaussPoints : Updated list of Gauss Integration values           [Ngauss,3]
+// Output      : Coordinates : Text file with coordinate values                  [Nnodes,3]
+//               Elements    : Text file with element values                     [Nelems,4]
+//               Solution    : Text file with solution values                    [Nnodes,1]
 //------------------------------------------------------------------------------------------------------------------------------------
 // Folder      : 
 //------------------------------------------------------------------------------------------------------------------------------------
