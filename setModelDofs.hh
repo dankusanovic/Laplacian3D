badd521d9bb9056350e6c751ab6c4c690de40d96
Danilo Kusanovic <danilo@danilo.cm.vtr.net>
2014-01-24 00:14:33 -0300

    Re-definition of DOFS includes Restraints

--- setModelDofs.hh
+++ setModelDofs.hh
@@ -12,7 +12,7 @@
   #ifndef SETMODELDOFS_HH
   #define SETMODELDOFS_HH 
 
-   void setModelDofs(int** &, int** &, int* &, int, int, int, int &, int &);
+   void setModelDofs(int** &, int* &, int* &, int , int , int &, int &);
 
   #endif
 
